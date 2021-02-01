883ddf1094824d5821794adc5b051efa4b3eda19
luismts <deivi_0326@hotmail.com>
2021-02-01 11:36:29 -0400

    Variable name changed

--- StickyHeader/StickyHeader/Controls/CustomCollectionView.cs
+++ StickyHeader/StickyHeader/Controls/CustomCollectionView.cs
@@ -40,9 +40,9 @@ namespace StickyHeader.Controls
         private async void GoodCollectionView_Scrolled(object sender, ItemsViewScrolledEventArgs e)
         {
             double scrollY = e.VerticalOffset < 0 ? 0 : e.VerticalOffset;
-            double maximumHeight = _customHeader.Height - _sticyHeader.Height;
+            double minimumHeight = _customHeader.Height - _sticyHeader.Height;
 
-            scrollY = scrollY > maximumHeight ? maximumHeight : scrollY;
+            scrollY = scrollY > minimumHeight ? minimumHeight : scrollY;
 
             //Show or hide the header and scroll the second view
             await Task.WhenAll(_customHeader?.TranslateTo(0, -scrollY, 50), 
